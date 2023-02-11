935a26edd722dede9fe397cdf4af0eeeeb3a0a0d
yazdanzv <yazdanzv.1378@gmail.com>
2023-02-11 13:25:08 +0330

    final changes

--- Heuristic.py
+++ Heuristic.py
@@ -6,8 +6,8 @@ import numpy as np
 class Heuristic:
     def __init__(self, board: np.array):
         self.board: np.array = board
-        self.rewards = {'three_connect_two_blanks': -10000, 'three_connect_one_blank': -2000,
-                        'two_connect_two_blanks': -50, 'two_connect_one_blank': -10}
+        self.rewards = {'four_connect': 2000, 'three_connect_two_blanks': 400, 'three_connect_one_blank': 50,
+                        'two_connect_two_blanks': 10, 'two_connect_one_blank': 5}
 
     def find_horizontal(self, length: int, player: int):  # Checked
         count_one_blank = 0
@@ -130,18 +130,25 @@ class Heuristic:
         return count_one_blank, count_two_blank
 
     def get_score_player(self, player: int):
+        # When its 4 it doesn't matter that it is one blank or two blanks but we used the same functions
+        four_connect_one_blank_H, four_connect_two_blanks_H = self.find_horizontal(4, player)
+        four_connect_one_blank_V, four_connect_two_blank_V = self.find_vertical(4, player)
+        four_connect_one_blank_D, four_connect_two_blank_D = self.find_diagonal(4, player)
         three_connect_one_blank_H, three_connect_two_blanks_H = self.find_horizontal(3, player)
         three_connect_one_blank_V, three_connect_two_blanks_V = self.find_vertical(3, player)
         three_connect_one_blank_D, three_connect_two_blanks_D = self.find_diagonal(3, player)
         two_connect_one_blank_H, two_connect_two_blanks_H = self.find_horizontal(2, player)
         two_connect_one_blank_V, two_connect_two_blanks_V = self.find_vertical(2, player)
         two_connect_one_blank_D, two_connect_two_blanks_D = self.find_diagonal(2, player)
+        four_connect = four_connect_one_blank_H + four_connect_two_blanks_H + four_connect_one_blank_V + \
+                       four_connect_two_blank_V + four_connect_one_blank_D + four_connect_two_blank_D
         three_connect_two_blanks = three_connect_two_blanks_H + three_connect_two_blanks_V + three_connect_two_blanks_D
         three_connect_one_blank = three_connect_one_blank_H + three_connect_one_blank_V + three_connect_one_blank_D
         two_connect_two_blanks = two_connect_two_blanks_H + two_connect_two_blanks_V + two_connect_two_blanks_D
         two_connect_one_blank = two_connect_one_blank_H + two_connect_one_blank_V + two_connect_one_blank_D
-        score = self.rewards['three_connect_two_blanks'] * three_connect_two_blanks + self.rewards[
-            'three_connect_one_blank'] * three_connect_one_blank + \
+        score = self.rewards['four_connect'] * four_connect + self.rewards[
+            'three_connect_two_blanks'] * three_connect_two_blanks + self.rewards[
+                    'three_connect_one_blank'] * three_connect_one_blank + \
                 self.rewards['two_connect_two_blanks'] * two_connect_two_blanks + self.rewards[
                     'two_connect_one_blank'] * two_connect_one_blank
         return score
